d11de61b93d1c5d140af8b17f61e965a33e6c21e
RimLuna <bougsrim@gmail.com>
2021-02-22 12:40:29 +0100

    ugh

--- shell.c
+++ shell.c
@@ -3,7 +3,11 @@
 void
 print_prompt ()
 {
-	_puts(PROMPT);
+	_puts("\e[1m\e[38;5;14m[");
+	_puts("\e[38;5;205m");
+	_puts("ikhane shell");
+	_puts("\e[38;5;14m] ");
+	_puts("\e[38;5;226m$ \e[0m");
 }
 
 int
